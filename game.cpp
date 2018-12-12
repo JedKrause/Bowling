a584308c642f04e1ab7e3d6e240b6f8661f823d0
jedkrause <jedkrause@mail.weber.edu>
2018-12-12 11:56:04 -0700

    Gutter Game Returns 0

--- game.cpp
+++ game.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "game.h"
+
+void Game::roll(int i)
+{
+
+}
+
+int Game::scoreGame()
+{
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
-
-TEST_CASE("TestName","[t]")
+#include "game.h"
+TEST_CASE("Does the Game exist","[score]")
 {
     //Arrange
-
+    Game *g = new Game;
     //Act
 
     //Assert
+    REQUIRE(g != NULL);
+}
 
+TEST_CASE("Gutter Game Return 0","[Score]")
+{
+    //Arrange
+    Game g;
+    int pins = 0;
+    //Act
+    for(int frame = 0; frame < 20; frame ++)
+    {
+        g.roll(pins);
+    }
+    //Assert
+    REQUIRE(0 == g.scoreGame());
 }
